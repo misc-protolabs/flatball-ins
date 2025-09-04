925fe98543279be5f86fe39a34969202b34d812f
mvschaefer <mvschaefer@gmail.com>
2025-09-04 04:28:03 -0700

    admin

--- dev/flatball-ahrs/lib/TaskScheduler/README.md
+++ dev/flatball-ahrs/lib/TaskScheduler/README.md
@@ -1,3 +1,19 @@
+<!-- 
+<!-- Copyright 2025 Michael V. Schaefer
+<!-- 
+<!-- Licensed under the Apache License, Version 2.0 (the "License");
+<!-- you may not use this file except in compliance with the License.
+<!-- You may obtain a copy of the License at:
+<!-- 
+<!--     http://www.apache.org/licenses/LICENSE-2.0
+<!-- 
+<!-- Unless required by applicable law or agreed to in writing, software
+<!-- distributed under the License is distributed on an "AS IS" BASIS,
+<!-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+<!-- See the License for the specific language governing permissions and
+<!-- limitations under the License.
+-->
+
 # Task Scheduler
 ### Cooperative multitasking for Arduino, ESPx, STM32 and other microcontrollers
 #### Version 3.8.5: 2024-06-17 [Latest updates](https://github.com/arkhipenko/TaskScheduler/wiki/Latest-Updates)
--- dev/flatball-ahrs/lib/TaskScheduler/examples/Scheduler_example05_StatusRequest/Scheduler_example05_StatusRequest.ino
+++ dev/flatball-ahrs/lib/TaskScheduler/examples/Scheduler_example05_StatusRequest/Scheduler_example05_StatusRequest.ino
@@ -1,3 +1,17 @@
+// Copyright 2025 Michael V. Schaefer
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 /** This test emulates querying 3 sensors once every 10 seconds, each could respond with a different delay
  *    (ultrasonic sensors for instance) and printing a min value of the three when all three have reported their values.
  *    The overall timeout of 1 second is setup as well.
