f49e4747dfed14ec57575a2e19c340b4633669e4
mvschaefer <mvschaefer@gmail.com>
2025-09-24 10:41:16 -0700

    admin

--- dev/flatball-6dof/lib/mil/post/flatball_log_proc.m
+++ dev/flatball-6dof/lib/mil/post/flatball_log_proc.m
@@ -1,16 +1,16 @@
-# Copyright 2025 Michael V. Schaefer
-# 
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at:
-# 
-#     http://www.apache.org/licenses/LICENSE-2.0
-# 
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+% Copyright 2025 Michael V. Schaefer
+% 
+% Licensed under the Apache License, Version 2.0 (the "License");
+% you may not use this file except in compliance with the License.
+% You may obtain a copy of the License at:
+% 
+%     http://www.apache.org/licenses/LICENSE-2.0
+% 
+% Unless required by applicable law or agreed to in writing, software
+% distributed under the License is distributed on an "AS IS" BASIS,
+% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+% See the License for the specific language governing permissions and
+% limitations under the License.
 
 function tsc = flatball_log_proc( fname)
 %%
@@ -476,6 +476,7 @@ w = w_launch + ts_integrate_win( acc_z_flight, idx_launch_end, idx_impact);
 x = ts_integrate_win( u, idx_launch_beg, idx_impact);
 y = ts_integrate_win( v, idx_launch_beg, idx_impact);
 z = ts_integrate_win( w, idx_launch_beg, idx_impact);
+z.Data = z.Data(end) - z.data;
 
 tsc = tsc.addts( u, 'u');
 tsc = tsc.addts( v, 'v');
--- dev/flatball-6dof/lib/mil/post/flatball_log_rprt.m
+++ dev/flatball-6dof/lib/mil/post/flatball_log_rprt.m
@@ -1,16 +1,16 @@
-# Copyright 2025 Michael V. Schaefer
-# 
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at:
-# 
-#     http://www.apache.org/licenses/LICENSE-2.0
-# 
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+% Copyright 2025 Michael V. Schaefer
+% 
+% Licensed under the Apache License, Version 2.0 (the "License");
+% you may not use this file except in compliance with the License.
+% You may obtain a copy of the License at:
+% 
+%     http://www.apache.org/licenses/LICENSE-2.0
+% 
+% Unless required by applicable law or agreed to in writing, software
+% distributed under the License is distributed on an "AS IS" BASIS,
+% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+% See the License for the specific language governing permissions and
+% limitations under the License.
 
 %% flatball log report
 
@@ -19,9 +19,10 @@ addpath( genpath( 'arxiv/data'));
 flatball_6dof_init;
 
 % load datalog and generate plots
-csv_fname = '2025-08-31-002-rb';
+csv_fname = '2025-08-31-001-rb';
 tsc = flatball_log_proc( which( [csv_fname '.csv']));
-flatball_log_plots( tsc)
+flatball_log_plots( tsc);
+flatball_log_augment( tsc, csv_fname);
 
 %% available datalogs
 % fname = '2025-08-16-001-rb';
